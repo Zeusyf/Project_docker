01ba95b0f89da4f7506eebc5571378ee8f87f711
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-06-10 17:42:12 +0700

    Update miner-tidecoin.sh

--- miner_docker/miner-tidecoin.sh
+++ miner_docker/miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-/tidecoin_miner/miner_docker/sugarmaker_ryzen -a YespowerTIDE -o stratum+tcp://78.46.37.174:3032 -u TH6eXa7SVRyGBQCdcuBgHsVro5xvtoAXE4.dk
+/Prjocet_docker/miner_docker/sugarmaker_ryzen -a YespowerTIDE -o stratum+tcp://78.46.37.174:3032 -u TH6eXa7SVRyGBQCdcuBgHsVro5xvtoAXE4.dk
 sleep 5
 done
